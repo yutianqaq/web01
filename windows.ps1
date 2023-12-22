ced817ebfebd36b14c79db3d72778a3f30c38e90
yutianqaq <40353095+yutianqaq@users.noreply.github.com>
2023-12-22 19:59:51 +0800

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,51 +1,51 @@
-@echo off
+function Get-SystemInformation {
+    Write-Host "系统版本："
+    systeminfo | findstr /B /C:"OS 名称" /C:"OS 版本"
+    Write-Host
 
-echo 系统版本：
-systeminfo | findstr /B /C:"OS 名称" /C:"OS 版本"
-echo.
+    Write-Host "KB补丁："
+    wmic qfe list brief
+    Write-Host
 
-echo KB补丁：
-wmic qfe list brief
-echo.
+    Write-Host "可能包含明文密码的文件："
+    findstr /si password *.txt *.ini
+    Write-Host
 
-echo 可能包含明文密码的文件：
-findstr /si password *.txt *.ini
-echo.
+    Write-Host "SSH密钥："
+    dir "$env:USERPROFILE\.ssh"
+    Write-Host
 
-echo SSH密钥：
-dir "%USERPROFILE%\.ssh"
-echo.
+    Write-Host "低权限用户可写的路径（不包括用户目录）："
+    icacls C:\ /t | findstr /i "(F)" | findstr /v /i "$env:USERPROFILE"
+    Write-Host
 
-echo 低权限用户可写的路径（不包括用户目录）：
-icacls C:\ /t | findstr /i "(F)" | findstr /v /i "%USERPROFILE%"
-echo.
+    Write-Host "PowerShell历史："
+    dir "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
+    Write-Host
 
-echo PowerShell历史：
-dir "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
-echo.
+    Write-Host "网络信息："
+    ipconfig /all
+    Write-Host
 
-echo 网络信息：
-ipconfig /all
-echo.
+    Write-Host "安全控制措施："
+    Write-Host "查询AMSI状态"
+    Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\AMSI"
 
-echo 安全控制措施：
-echo 查询AMSI状态
-Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\AMSI"
+    Write-Host "查询AppLocker配置"
+    Get-AppLockerPolicy -Effective | Format-List *
 
-echo 查询AppLocker配置
-Get-AppLockerPolicy -Effective | Format-List *
+    Write-Host "查询LAPS情况"
+    Get-AdmPwdPasswordSetting -Effective | Format-Table *
 
-echo 查询LAPS情况
-Get-AdmPwdPasswordSetting -Effective | Format-Table *
+    Write-Host "查询PPL设置"
+    Get-ProcessMitigation -System | Format-List *
 
-echo 查询PPL设置
-Get-ProcessMitigation -System | Format-List *
+    Write-Host "查询UAC设置"
+    Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA"
 
-echo 查询UAC设置
-Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA"
+    Write-Host "查询约束语言模式设置"
+    Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Nls\Language" -Name "RestrictSystemChange"
 
-echo 查询约束语言模式设置
-Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Nls\Language" -Name "RestrictSystemChange"
-
-$ExecutionContext.SessionState.LanguageMode
-echo.
+    Write-Host $ExecutionContext.SessionState.LanguageMode
+    Write-Host
+}
